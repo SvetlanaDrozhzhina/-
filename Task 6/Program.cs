b11b340330716a4f8cd96d7db1c916230ebb98d6
Дрожжина Светлана <arianna-14@mail.ru>
2023-03-24 23:46:11 +0300

    task 6

---
+++ Task 6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число:  ");
+int number = int.Parse(Console.ReadLine()!);
+int remDiv = number % 2;
+if(remDiv == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
