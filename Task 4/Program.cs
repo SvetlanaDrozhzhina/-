0885c3f857005bf20384e0aba24e69f08955b735
Дрожжина Светлана <arianna-14@mail.ru>
2023-03-24 23:38:02 +0300

    task 4

---
+++ Task 4/Program.cs
@@ -0,0 +1,20 @@
+﻿int max = 0;
+Console.WriteLine("Введите первое число:  ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число:  ");
+int b = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите третье число:  ");
+int e = int.Parse(Console.ReadLine()!);
+if (a > max)
+{
+    max = a;
+}
+if (b > max)
+{
+    max = b;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine("max = " + max);
