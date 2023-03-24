66cedffcfea24399a3e5d6d743a3e92c05720d3b
Дрожжина Светлана <arianna-14@mail.ru>
2023-03-24 23:55:34 +0300

    task 8

---
+++ Task 8/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число N: "); 
+int N = int.Parse(Console.ReadLine()!); 
+int number = 2;
+if (N > 1)
+{
+    while(number <= N)
+    {
+        Console.Write(number + " ");
+        number = number +2;
+    }
+}
