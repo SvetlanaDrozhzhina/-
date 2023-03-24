f969db6cd383db5ced2ccb81e37ac97573472f90
Дрожжина Светлана <arianna-14@mail.ru>
2023-03-24 22:59:31 +0300

    task 2

---
+++ Task 2/Program.cs
@@ -0,0 +1,17 @@
+﻿int max = 0;
+int min = 0;
+Console.WriteLine("Введите первое число:  ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число:  ");
+int b = int.Parse(Console.ReadLine()!);
+if(a > b)
+{
+    max = a;
+    min = b;
+}
+else
+{
+    max = b;
+    min = a;
+}
+Console.WriteLine("max = " + max + " и " + "min = " + min);
