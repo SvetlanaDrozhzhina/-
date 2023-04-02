0690d0ee4fe95d60e8ecb3312d284b27bcb9a033
Дрожжина Светлана <arianna-14@mail.ru>
2023-04-02 18:13:32 +0300

    task 10

---
+++ Task 10/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine ("Введи трёхзначное число: ");
+try {
+    int Value = Convert.ToInt32(Console.ReadLine());
+    int Length = Value.ToString().Length;
+    if (Length == 3) {
+        int result = ( Value/10 ) % 10;
+        Console.WriteLine($"Вторая цифра {result}");
+        } else {
+                Console.WriteLine("Введено не 3-х значное число");
+        }
+} catch (System.Threading.ThreadAbortException) {
+	System.Threading.Thread.ResetAbort();
+} catch (Exception e) {
+	Console.WriteLine(e.Message);
+}
